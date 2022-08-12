61723e80d969846aca1ec042d081026adb3ea2cf
Shritik <shritikraj@hotmail.com>
2022-08-12 14:26:19 +0530

    Final changes

--- 24-Model-Deployment/mynewapi.py
+++ 24-Model-Deployment/mynewapi.py
@@ -1,31 +1,21 @@
 from flask import Flask,request,jsonify
 import joblib
 import pandas as pd
-
-# CREATE FLASK APP
-app = Flask(__name__)
-
-
-# CONNECT POST API CALL ---> predict() Function  #http://localhost:5000/predict
-@app.route('/predict',methods=['POST'])
+#Create Flask app
+app=Flask(__name__)
+#Connect post Api call-->predict function
+@app.route('/predict',methods=['POST'])#http://localhost:5000/predict
 def predict():
-    
-    # GET JSON REQUEST
-    feat_data = request.json
-    # CONVERT JSON to PANDAS DF (col names)
-    df = pd.DataFrame(feat_data)
-    df = df.reindex(columns=col_names)
-    # PREDICT
-    prediction = list(model.predict(df))
-    
-    
-    return jsonify({'prediction':str(prediction)})
-
-
-# LOAD MY MODEL and LOAD COLUMN NAMES
-if __name__ == '__main__':
-    
-    model = joblib.load("final_model.pkl")
-    col_names = joblib.load('column_names.pkl')
-    
+    #GET JSON Request
+    feat_data=request.json
+    #Convert to pandas df(matches column names)
+    df=pd.DataFrame(feat_data)
+    df=df.reindex(columns=col_names)
+    #Predict json
+    prediction=list(model.predict(df))
+    return jsonify({'prediction':str(prediction)})#Prediction
+#load model and setup column names
+if __name__=='__main__':
+    model=joblib.load('final_model.pkl')
+    col_names=joblib.load('column_names.pkl')
     app.run(debug=True)
